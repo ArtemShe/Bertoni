7e687267b93ff8f8071d30e37b5148bec78349c0
ArtemShe <68066163+ArtemShe@users.noreply.github.com>
2020-07-13 17:19:28 +0300

    Add files via upload

---
+++ DataAccess/Entries/PhotoComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entries
+{
+    public class PhotoComment
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+    }
+}
